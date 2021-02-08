8b7e9a8105be3ae5352c91691b894df4b5f9fa98
Yohann76 <yohanndurand76@gmail.com>
2021-02-08 17:14:18 +0100

    [SearchAPI] add button for search enterprise with siren

--- webapp/src/views/Home.vue
+++ webapp/src/views/Home.vue
@@ -8,57 +8,56 @@
 
     <h2> Recherche par numéro de Siren :  </h2>
 
-    <input v-model="Siren" placeholder="edit me">
-    <p>Siren is: {{ Siren }}</p>
+    <!-- <input v-model="SirenData" placeholder="Siren"> 
+    <p>Siren is: {{ SirenData }}</p> --> 
+    <!-- <button v-click="sirenFormRequest">Search</button> -->
+
+    <form @submit.prevent="SearchSiren">
+      <input type="text" v-model="SirenData"> 
+      <button type="submit">
+          Search
+      </button>
+    </form>
+    <p>Siren is: {{ SirenData }}</p>  
+
+    {{ SirenRes }}
+     
     <!-- https://api.insee.fr/entreprises/sirene/V3/siren/005520135 -->
+    
 
-
-    <h2> ensemble des centre de formations en Normandie <!-- Todo : create form for select activite and department--> </h2>
+    <h2> Ensemble des centres de formations en Normandie <!-- Todo : create form for select activite and department--> </h2>
     <!-- 
     https://api.insee.fr/entreprises/sirene/V3/siret?q=(activitePrincipaleUniteLegale:85.59A)AND(codeCommuneEtablissement:[76000 TO 76999]) // formation adulte && 76 
     -->
 
-    <!--
-    Domaine : 
-    <input v-model="Domaine" placeholder="edit me">
-    <p>Domaine is: {{ Domaine }}</p>
+    <!-- good request -->
+    
+    <div class="container"> 
+      <table class="table">
+        <tbody>
+          <div v-for="item in InseeAPI" :key="item">
+            <tr>
+              <th scope="row"> # </th>
+              <td>{{ item.siren }}</td>
+              <td>{{ item.siret }}</td>
+              <td>{{ item.uniteLegale.denominationUniteLegale }} </td>
+              <td>{{ item.adresseEtablissement.libelleCommuneEtablissement }} </td>
+              <td>{{ item.uniteLegale.activitePrincipaleUniteLegale }} </td>
+            </tr>
+          </div>
+        </tbody>
+      </table>
+    </div>
+    
 
-    Numéro de département : 
-    <input v-model="departement" placeholder="edit me">
-    <p>département is: {{ departement }}</p>
-    -->
-
-  <div class="container"> 
-    <table class="table">
-      <tbody>
-        <div v-for="item in InseeAPI" :key="item">
-          <tr>
-            <th scope="row"> # </th>
-            <td>{{ item.siren }}</td>
-            <td>{{ item.siret }}</td>
-            <td>{{ item.uniteLegale.denominationUniteLegale }} </td>
-            <td>{{ item.adresseEtablissement.libelleCommuneEtablissement }} </td>
-            <td>{{ item.uniteLegale.activitePrincipaleUniteLegale }} </td>
-          </tr>
-        </div>
-      </tbody>
-    </table>
-  </div>
-
-  {{ InseeAPI }}
-
-
-
-    <h2> Ensemble des entreprise FR dans la santé, avec un CA suppérieur à 120 Millions  </h2>
+    <h2> Ensemble des entreprises FR dans la santé, avec un CA suppérieur à 120 Millions  </h2>
     <!-- 
       https://api.insee.fr/entreprises/sirene/V3/siret?q=activitePrincipaleUniteLegale:86.10Z  // activité = activités hospitalières
     -->
-
-    <h2> PME dans un rayon de 300km de Fecamp qui ont + de 150 salarié </h2>
+    <h2> PME dans un rayon de 300km de Fecamp qui ont + de 150 salariés </h2>
     <!-- https://api.insee.fr/entreprises/sirene/V3/siret?q=codeCommuneEtablissement:76400 -->
     <!-- https://api.insee.fr/entreprises/sirene/V3/siret?q=categorieEntreprise:PME --> <!-- Recherche par PME-->
     <!-- https://api.insee.fr/entreprises/sirene/V3/siret?q=trancheEffectifsUniteLegale:12  // nb salarié ?--> 
-
     <!-- test all data-->
     <!-- https://api.insee.fr/entreprises/sirene/V3/siret?q=(categorieEntreprise:PME)AND(codeCommuneEtablissement:76400)&=trancheEffectifUniteLegal:11 -->
 
@@ -81,19 +80,54 @@ import axios from 'axios'
           InseeAPI: '',
           Domaine: '',
           department: '',
-          Siren:'',
+          SirenData: '',
+          SirenRes: '',
       }
   },
   methods: {
     testMethods(e) {
       console.log("testMethods");
-    }
-  },
+    },
+    SearchSiren: function() {
+      console.log(this.SirenData); 
+
+     const TOKEN = '17e30c48-3d17-394d-b224-72611bcab21f'; // Token Test
+     const BASEURL = 'https://api.insee.fr/entreprises/sirene/V3';
+     const ENDPOINT =  '/{this.SirenData}';
+     
+     axios.create({
+           baseURL: BASEURL,
+           headers: {
+               'Content-Type': 'application/json',
+               'Authorization': 'Bearer '+TOKEN,
+               //'Access-Control-Allow-Origin': '*'
+           }
+       })
+       .get(ENDPOINT)
+       .then(res => {
+               console.log(res);
+               this.SirenRes = res
+       }
+    ); // end axios
+
+    
+      /*
+      axios.post('https://api.insee.fr/entreprises/sirene/V3/siren/{{SirenData}}', {SirenData: this.SirenData})
+        .then(res => {
+          // do something with res
+        })
+        .catch(err => {
+          // catch error});
+        })
+        */
+    },
+  }, // end methods
   createdtest: async function(){
       const gResponse = await fetch("http://localhost:5000/greeting");
       const gObject = await gResponse.json();
       this.APImessageGreeting = gObject.greeting; // greeting1 for other request in JSON
   },
+  // methods for get siren : region && activity
   created: async function(){
      // console.log ("insee method as executed")
 
@@ -112,9 +146,9 @@ import axios from 'axios'
        .then(res => {
                console.log(res.data.etablissements);
                this.InseeAPI = res.data.etablissements
-       });
-    
-  },
+       }
+    ); // end axios
+  }, // end created
 
   } // end export 
 </script>
