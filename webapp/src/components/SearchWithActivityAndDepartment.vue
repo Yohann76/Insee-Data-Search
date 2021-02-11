d3b177b0d47124115b32eedef7478ebec52b4e43
Yohann76 <yohanndurand76@gmail.com>
2021-02-11 10:00:17 +0100

    [Axios] update request with new select

--- webapp/src/components/SearchWithActivityAndDepartment.vue
+++ webapp/src/components/SearchWithActivityAndDepartment.vue
@@ -30,6 +30,27 @@
       </select>
       <br/>
 
+      <!-- select Type entreprise -->
+      <p> Select Nb salarié</p>
+      <select v-model="selectedEffectifs" class="form-control">
+        <option value="01">1 ou 2 salariés</option>
+        <option value="02">3 à 5 salariés</option>
+        <option value="03">6 à 9 salariés</option>
+        <option value="11">10 à 19 salariés</option>
+        <option value="12">20 à 49 salariés</option>
+        <option value="21">50 à 99 salariés</option>
+        <option value="22">100 à 199 salariés</option>
+        <option value="31">200 à 249 salariés</option>
+        <option value="32">250 à 499 salariés</option>
+        <option value="41">500 à 999 salariés</option>
+        <option value="42">1 000 à 1 999 salariés</option>
+        <option value="51">2 000 à 4 999 salariés</option>
+        <option value="52">5 000 à 9 999 salariés</option>
+        <option value="53">10 000 salariés et plus</option>
+      </select>
+      <br/>
+
+      <br/>
       <!-- submit-->
       <button type="submit" class="btn btn-info">
           Search
@@ -41,6 +62,8 @@
 
     <br/>
     <!-- display request -> Search activity and region -->
+
+    {{ infoRequest }}
 
     <div class="container"> 
       <table class="table">
@@ -73,21 +96,20 @@ import axios from 'axios'
           selectedActivity: '',
           selectedDepartment: '',
           selectedType: '',
+          selectedEffectifs: '',
           ListSiretActivityDepartment : '', // result 
           nbResult : '', // result 
+          infoRequest : '', // info 
       }
   },
   methods: {
     SearchWithActivityAndRegion: function(){
 
-      console.log("SearchWithActivityAndRegion");
       // axios request 
       const TOKEN = '17e30c48-3d17-394d-b224-72611bcab21f';
 
-      console.log(this.selectedActivity);
-      console.log(this.selectedDepartment);
-
-      axios.get('https://api.insee.fr/entreprises/sirene/V3/siret?q=(activitePrincipaleUniteLegale:' +this.selectedActivity+')AND(codeCommuneEtablissement:['+this.selectedDepartment+'000 TO '+this.selectedDepartment+'999])AND(categorieEntreprise:'+this.selectedType+')', {
+      // exemple request : https://api.insee.fr/entreprises/sirene/V3/siret?q=(activitePrincipaleUniteLegale:85.59A)AND(codeCommuneEtablissement:[76000 TO 76999])AND(categorieEntreprise:PME)AND(trancheEffectifsUniteLegale:21)
+      axios.get('https://api.insee.fr/entreprises/sirene/V3/siret?q=(activitePrincipaleUniteLegale:' +this.selectedActivity+')AND(codeCommuneEtablissement:['+this.selectedDepartment+'000 TO '+this.selectedDepartment+'999])AND(categorieEntreprise:'+this.selectedType+')AND(trancheEffectifsUniteLegale:'+this.selectedEffectifs+')', {
           headers: {
             'Content-Type': 'application/json',
             'Authorization': 'Bearer '+TOKEN,
@@ -98,9 +120,12 @@ import axios from 'axios'
           console.log(this.ListSiretActivityDepartment);
 
           this.nbResult = res.data.header.nombre;
+          this.infoRequest = ''
         })
         .catch(err => {
           console.log("request non valide");
+          this.infoRequest = 'Aucun établissement à été trouvé'
+          this.ListSiretActivityDepartment = '';
         })
       // end axios request 
     }, // end function
